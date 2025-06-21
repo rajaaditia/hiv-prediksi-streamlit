e660452c28de8b2d1b349f4ca54003c4efe5ea4b
rajaaditia <147203880+rajaaditia@users.noreply.github.com>
2025-06-21 19:07:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ if uploaded_file:
     y = data_sampled['Result']
 
     X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.2, random_state=42, stratify=y)
+        X, y, test_size=0.4, random_state=42, stratify=y)
 
     # BALANCING HANYA DATA TRAINING
     train_df = pd.concat([X_train, y_train], axis=1)
